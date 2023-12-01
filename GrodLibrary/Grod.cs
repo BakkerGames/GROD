0ed83fc537ab600352424940d412211583b86b0b
Scott Bakker <sbakker343@gmail.com>
2023-11-30 20:17:49 -0500

    Version 1.0.0

---
+++ GrodLibrary/Grod.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GrodLibrary;
+
+/// <summary>
+/// GROD - Game Resource Overlay Dictionary
+/// </summary>
+public class Grod : IDictionary<string, string>
+{
+    private readonly Dictionary<string, string> _base = [];
+    private readonly Dictionary<string, string> _overlay = [];
+
+    public bool UseOverlay { get; set; } = false;
+
+    public string this[string key]
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException(nameof(key));
+            if (UseOverlay && _overlay.TryGetValue(key, out string? value1))
+                return value1;
+            else if (_base.TryGetValue(key, out string? value2))
+                return value2;
+            else
+                return "";
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException(nameof(key));
+            if (UseOverlay)
+            {
+                if (!_overlay.TryAdd(key, value))
+                    _overlay[key] = value;
+            }
+            else
+            {
+                if (!_base.TryAdd(key, value))
+                    _base[key] = value;
+            }
+        }
+    }
+
+    public ICollection<string> Keys =>
+        UseOverlay ? _base.Keys.Union(_overlay.Keys).ToList() : _base.Keys;
+
+    public ICollection<string> KeysOverlay =>
+        _overlay.Keys;
+
+    public ICollection<string> Values =>
+        UseOverlay ? FlattenValues() : _base.Values;
+
+    private List<string> FlattenValues()
+    {
+        List<string> result = [];
+        foreach (string key in Keys)
+            result.Add(this[key]);
+        return result;
+    }
+
+    public int Count =>
+        (UseOverlay ? _base.Keys.Union(_overlay.Keys) : _base.Keys).Count();
+
+    public bool IsReadOnly => false;
+
+    public void Add(string key, string value)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentNullException(nameof(key));
+        this[key] = value;
+    }
+
+    public void Add(KeyValuePair<string, string> item)
+    {
+        if (string.IsNullOrWhiteSpace(item.Key))
+            throw new ArgumentNullException(nameof(item));
+        this[item.Key] = item.Value;
+    }
+
+    public void Clear()
+    {
+        if (UseOverlay)
+            _overlay.Clear();
+        _base.Clear();
+    }
+
+    public void ClearOverlay()
+    {
+        _overlay.Clear();
+    }
+
+    public bool Contains(KeyValuePair<string, string> item)
+    {
+        if (string.IsNullOrWhiteSpace(item.Key))
+            throw new ArgumentNullException(nameof(item));
+        return _base.Contains(item) || (UseOverlay && _overlay.Contains(item));
+    }
+
+    public bool ContainsKey(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentNullException(nameof(key));
+        return _base.ContainsKey(key) || (UseOverlay && _overlay.ContainsKey(key));
+    }
+
+    public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+    {
+        var i = 0;
+        foreach (string key in Keys)
+        {
+            array[i + arrayIndex] = new KeyValuePair<string, string>(key, this[key]);
+            i++;
+        }
+    }
+
+    public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+    {
+        List<KeyValuePair<string, string>> result = [];
+        foreach (string key in Keys)
+            result.Add(new KeyValuePair<string, string>(key, this[key]));
+        return (IEnumerator<KeyValuePair<string, string>>)result;
+    }
+
+    public bool Remove(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentNullException(nameof(key));
+        bool result = _base.Remove(key);
+        if (UseOverlay)
+            result |= _overlay.Remove(key);
+        return result;
+    }
+
+    public bool Remove(KeyValuePair<string, string> item)
+    {
+        if (string.IsNullOrWhiteSpace(item.Key))
+            throw new ArgumentNullException(nameof(item));
+        bool result = _base.Contains(item) && _base.Remove(item.Key);
+        if (UseOverlay)
+            result |= _overlay.Contains(item) && _overlay.Remove(item.Key);
+        return result;
+    }
+
+    public bool TryGetValue(string key, [MaybeNullWhen(false)] out string value)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentNullException(nameof(key));
+        if (UseOverlay && _overlay.ContainsKey(key))
+            return _overlay.TryGetValue(key, out value);
+        else
+            return _base.TryGetValue(key, out value);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return (IEnumerator)(UseOverlay ? _base.Union(_overlay) : _base);
+    }
+}
---
+++ TestGrod/UnitTestGrod.cs
@@ -0,0 +1,308 @@
+using GrodLibrary;
+
+namespace TestGrod;
+
+public class Tests
+{
+    [Test]
+    public void TestNullKey()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        try
+        {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            g[null] = "null";
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            Assert.Fail();
+        }
+        catch (Exception)
+        {
+            Assert.Pass();
+        }
+    }
+
+    [Test]
+    public void TestEmptyKey()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        try
+        {
+            g[""] = "empty";
+            Assert.Fail();
+        }
+        catch (Exception)
+        {
+            Assert.Pass();
+        }
+    }
+
+    [Test]
+    public void TestWhitespaceKey()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        try
+        {
+            g["   \t\r\n  "] = "whitespace";
+            Assert.Fail();
+        }
+        catch (Exception)
+        {
+            Assert.Pass();
+        }
+    }
+
+    [Test]
+    public void TestNotFound()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        Assert.That(g["k"], Is.EqualTo(""));
+    }
+
+    [Test]
+    public void TestSingleValue()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["k"] = "v";
+        Assert.That(g["k"], Is.EqualTo("v"));
+    }
+
+    [Test]
+    public void TestCaseSensitiveKeys()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["k"] = "v";
+        Assert.That(g["K"], Is.EqualTo(""));
+    }
+
+    [Test]
+    public void TestOverlayGetOverlayBaseThru()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["k"] = "v";
+        g.UseOverlay = true;
+        Assert.That(g["k"], Is.EqualTo("v"));
+    }
+
+    [Test]
+    public void TestOverlayGetOverlayValue()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["k"] = "v";
+        g.UseOverlay = true;
+        g["k"] = "value";
+        Assert.That(g["k"], Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void TestOverlayGetOverlayBackToBase()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["k"] = "v";
+        g.UseOverlay = true;
+        g["k"] = "value";
+        g.UseOverlay = false;
+        Assert.That(g["k"], Is.EqualTo("v"));
+    }
+
+    [Test]
+    public void TestGetKeys()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        var answer = "";
+        foreach (string s in g.Keys)
+        {
+            answer += s;
+            answer += g[s];
+        }
+        // test separately, order is not guaranteed
+        Assert.That(answer.Contains("a1") && answer.Contains("b2") && answer.Contains("c3"), Is.True);
+    }
+
+    [Test]
+    public void TestGetKeysBaseAndOverlay()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g.UseOverlay = true;
+        g["c"] = "3";
+        var answer = "";
+        foreach (string s in g.Keys)
+        {
+            answer += s;
+            answer += g[s];
+        }
+        // test separately, order is not guaranteed
+        Assert.That(answer.Contains("a1") && answer.Contains("b2") && answer.Contains("c3"), Is.True);
+    }
+
+    [Test]
+    public void TestGetKeysPrefix()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        var answer = "";
+        foreach (string s in g.Keys.Where(x => x.StartsWith('b')))
+        {
+            answer += s;
+            answer += g[s];
+        }
+        Assert.That(answer, Is.EqualTo("b2"));
+    }
+
+    [Test]
+    public void TestRemoveKey()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        g.Remove("b");
+        var answer = "";
+        foreach (string s in g.Keys)
+        {
+            answer += s;
+            answer += g[s];
+        }
+        // test separately, order is not guaranteed
+        Assert.That(answer.Contains("a1") && !answer.Contains("b2") && answer.Contains("c3"), Is.True);
+    }
+
+    [Test]
+    public void TestContainsKey()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        Assert.That(g.ContainsKey("a") && g.ContainsKey("b") && g.ContainsKey("c"), Is.True);
+    }
+
+    [Test]
+    public void TestContainsKeyAfterRemove()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        g.Remove("b");
+        Assert.That(g.ContainsKey("a") && !g.ContainsKey("b") && g.ContainsKey("c"), Is.True);
+    }
+
+    [Test]
+    public void TestContainsKeyAny()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g.UseOverlay = true;
+        g["b"] = "2";
+        Assert.That(g.ContainsKey("a") && g.ContainsKey("b"), Is.True);
+    }
+
+    [Test]
+    public void TestMissingKeyNotFound()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        Assert.That(g.ContainsKey("a"), Is.False);
+    }
+
+    [Test]
+    public void TestOverlayContainsKeyInBase()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g.UseOverlay = true;
+        Assert.That(g.ContainsKey("a"), Is.True);
+    }
+
+    [Test]
+    public void TestModifyValue()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["a"] = "123456";
+        var answer = g["a"];
+        Assert.That(answer, Is.EqualTo("123456"));
+    }
+
+    [Test]
+    public void TestClear()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        g.Clear();
+        var answer = g.Count;
+        Assert.That(answer, Is.Zero);
+    }
+
+    [Test]
+    public void TestClearOverlay()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        g.UseOverlay = true;
+        g["a"] = "111";
+        g["b"] = "222";
+        g["c"] = "333";
+        var answerBeforeClear = g.Count;
+        g.Clear();
+        var answerAfterClear = g.Count;
+        Assert.That(answerBeforeClear == 3 && answerAfterClear == 0, Is.True);
+    }
+
+    [Test]
+    public void TestKeysCountMatchesValuesCountBase()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        Assert.That(g.Keys, Has.Count.EqualTo(g.Values.Count));
+    }
+
+    [Test]
+    public void TestKeysCountMatchesValuesCountOverlay()
+    {
+        Grod g = [];
+        g.UseOverlay = false;
+        g["a"] = "1";
+        g["b"] = "2";
+        g["c"] = "3";
+        g.UseOverlay = true;
+        g["a"] = "111";
+        g["b"] = "222";
+        g["c"] = "333";
+        Assert.That(g.Keys, Has.Count.EqualTo(g.Values.Count));
+    }
+}
